1df87bdc12e243f8ec2a50f6d0bd6e2ab25a8fad
Kostia <aks20050504@gmail.com>
2024-11-27 00:27:30 +0200

    finish: lab2

---
+++ Lab2/Lab-OOP/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ArithmeticProgressionApp
+{
+    class ArithmeticProgression
+    {
+        private int _firstTerm; 
+        private int _difference; 
+        private int _numTerms; 
+
+        public void InputData()
+        {
+            Console.Write("(a): ");
+            _firstTerm = int.Parse(Console.ReadLine());
+
+            Console.Write("(d): ");
+            _difference = int.Parse(Console.ReadLine());
+
+            Console.Write("(n): ");
+            _numTerms = int.Parse(Console.ReadLine());
+
+            if (_numTerms <= 0)
+            {
+                throw new ArgumentException("Incorrect number.");
+            }
+        }
+
+        public int CalculateSum()
+        {
+            return (_numTerms * (2 * _firstTerm + (_numTerms - 1) * _difference)) / 2;
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+                ArithmeticProgression progression = new ArithmeticProgression();
+
+                progression.InputData();
+
+                int sum = progression.CalculateSum();
+                Console.WriteLine($"sum of the arithmetic progression is: {sum}");
+        }
+    }
+}
